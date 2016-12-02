c88cd5580c10f0c138185f60c86025dde346e3f5
Suyesh Bhandari <suyeshb@gmail.com>
2016-12-01 22:59:26 -0500

    Some commandline tweaks

--- lib/ocr_space.rb
+++ lib/ocr_space.rb
@@ -1,3 +1,9 @@
 require "ocr_space/version"
 require "ocr_space/from_url"
 require "ocr_space/from_file"
+
+module OcrSpace
+  def text_data(result)
+    result.data.parsed_response["ParsedResults"][0]["ParsedText"]
+  end
+end
