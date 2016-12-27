194db74941ac528e1e616d822a77691518da21b3
Suyesh Bhandari <suyeshb@gmail.com>
2016-12-27 09:29:45 -0500

    Refactor

--- lib/ocr_space/from_file.rb
+++ lib/ocr_space/from_file.rb
@@ -1,10 +1,9 @@
 require 'ocr_space/file_post'
-require 'fileutils'
 
 module OcrSpace
     class FromFile
         attr_reader :data
-        def initialize(apikey: ENV['ocr_api_key'], language: 'eng', isOverlayRequired: false, files: nil, ocr_space: 'https://api.ocr.space/parse/image')
+        def initialize(apikey: ENV['ocr_api_key'], language: 'eng', isOverlayRequired: false, files: nil)
             @file = File.new(files)
             @data = OcrSpace::FilePost.post('/parse/image',
                                             body: { apikey: apikey,
@@ -15,7 +14,7 @@ module OcrSpace
         end
 
         def text_data
-            data[0]['ParsedText'].gsub(/(\\r|\\n)/, "")
+            data[0]['ParsedText'].delete("\n").delete("\r").strip
         end
     end
 end
--- lib/ocr_space/from_url.rb
+++ lib/ocr_space/from_url.rb
@@ -4,7 +4,7 @@ module OcrSpace
     class FromUrl
         attr_reader :data
         def initialize(apikey: ENV['ocr_api_key'], language: 'eng', isOverlayRequired: false, url: nil, ocr_space: 'https://api.ocr.space/parse/image')
-            @data = HTTParty.post('https://api.ocr.space/parse/image',
+            @data = HTTParty.post(ocr_space,
                                   body: { apikey: apikey,
                                           language: language,
                                           isOverlayRequired: isOverlayRequired,
@@ -13,7 +13,7 @@ module OcrSpace
         end
 
         def text_data
-            data[0]['ParsedText'].gsub(/(\\r|\\n)/, "")
+            data[0]['ParsedText'].delete("\n").delete("\r").strip
         end
     end
 end
