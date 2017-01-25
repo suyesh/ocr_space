f048125259555933950ce4bad73b4a0d84822341
Suyesh Bhandari <suyeshb@gmail.com>
2017-01-25 12:33:06 -0500

    Bugfix

--- lib/ocr_space/convert.rb
+++ lib/ocr_space/convert.rb
@@ -2,7 +2,7 @@ require 'ocr_space/file_post'
 
 module OcrSpace
     module Convert
-        def convert(apikey: @api_key, language: 'eng', isOverlayRequired: false, file: nil, url: nil, clean_result: false)
+        def convert(apikey: @api_key, language: 'eng', isOverlayRequired: false, file: nil, url: nil)
           if file
             @files = File.new(file)
             @data = OcrSpace::FilePost.post('/parse/image',
@@ -11,7 +11,6 @@ module OcrSpace
                                                     isOverlayRequired: isOverlayRequired,
                                                     file: @files })
             @data = @data.parsed_response['ParsedResults']
-            clean_result == true ? @data[0]['ParsedText'].delete("\n").delete("\r").strip : @data = @data.parsed_response['ParsedResults']
           elsif url
             @data = HTTParty.post('https://api.ocr.space/parse/image',
                                   body: { apikey: apikey,
@@ -19,7 +18,6 @@ module OcrSpace
                                           isOverlayRequired: isOverlayRequired,
                                           url: url })
             @data = @data.parsed_response['ParsedResults']
-            clean_result == true ? @data[0]['ParsedText'].delete("\n").delete("\r").strip : @data = @data.parsed_response['ParsedResults']
           else
             "You need to Pass either file or url."
           end
